0323d1efcb5790982e2fe91174c5335debc82e0e
stefan-hoeck <hock@zhaw.ch>
2023-12-02 05:42:13 +0100

    [ lint ] use tabs in Makefiles

--- data/gencode.sh
+++ data/gencode.sh
@@ -3,7 +3,7 @@
 make -C codegen all
 codegen/error_gen >src/System/UV/Data/Error.idr
 
-cat > src/System/UV/Data/RunMode.idr <<EOT
+cat >src/System/UV/Data/RunMode.idr <<EOT
 module System.UV.Data.RunMode
 
 import Derive.Prelude
@@ -25,7 +25,7 @@ EOT
 
 codegen/run_mode_gen >>src/System/UV/Data/RunMode.idr
 
-cat > src/System/UV/Data/Pointer.idr <<EOT
+cat >src/System/UV/Data/Pointer.idr <<EOT
 module System.UV.Data.Pointer
 
 %default total
@@ -33,7 +33,7 @@ EOT
 
 codegen/size_gen >>src/System/UV/Data/Pointer.idr
 
-cat > src/System/UV/Data/DNS.idr <<EOT
+cat >src/System/UV/Data/DNS.idr <<EOT
 module System.UV.Data.DNS
 
 import Derive.Prelude
@@ -90,9 +90,9 @@ data Protocol : Type where
 %runElab derive "Protocol" [Show,Eq]
 EOT
 
-codegen/dns_gen >> src/System/UV/Data/DNS.idr
+codegen/dns_gen >>src/System/UV/Data/DNS.idr
 
-cat > src/System/UV/Data/File.idr << EOT
+cat >src/System/UV/Data/File.idr << EOT
 module System.UV.Data.File
 
 import Derive.Prelude
