78a8e8e8896b175f253b9f079826696558a0f082
stefan-hoeck <hock@zhaw.ch>
2023-12-02 05:09:15 +0100

    [ lint ] use zd for size literals

--- data/codegen/size_gen.c
+++ data/codegen/size_gen.c
@@ -4,35 +4,35 @@
 #include <uv.h>
 
 int main(void) {
-  #define XX(code, tpe) printf("\npublic export %%inline\nSZ_" #code " : Bits32\nSZ_" #code " = %d\n", uv_handle_size(UV_ ## code));
+  #define XX(code, tpe) printf("\npublic export %%inline\nSZ_" #code " : Bits32\nSZ_" #code " = %zd\n", uv_handle_size(UV_ ## code));
   UV_HANDLE_TYPE_MAP(XX)
   #undef XX
-  #define XX(code, tpe) printf("\npublic export %%inline\nSZ_" #code " : Bits32\nSZ_" #code " = %d\n", uv_req_size(UV_ ## code));
+  #define XX(code, tpe) printf("\npublic export %%inline\nSZ_" #code " : Bits32\nSZ_" #code " = %zd\n", uv_req_size(UV_ ## code));
   UV_REQ_TYPE_MAP(XX)
   #undef XX
 
   printf("\npublic export %%inline\n");
   printf("SZ_BUF : Bits32\n");
-  printf("SZ_BUF = %d\n", sizeof(uv_buf_t));
+  printf("SZ_BUF = %zd\n", sizeof(uv_buf_t));
 
   printf("\npublic export %%inline\n");
   printf("SZ_LOOP : Bits32\n");
-  printf("SZ_LOOP = %d\n", sizeof(uv_loop_t));
+  printf("SZ_LOOP = %zd\n", sizeof(uv_loop_t));
 
   printf("\npublic export %%inline\n");
   printf("SZ_SOCKADDR_IN : Bits32\n");
-  printf("SZ_SOCKADDR_IN = %d\n", sizeof(struct sockaddr_in));
+  printf("SZ_SOCKADDR_IN = %zd\n", sizeof(struct sockaddr_in));
 
   printf("\npublic export %%inline\n");
   printf("SZ_SOCKADDR_IN6 : Bits32\n");
-  printf("SZ_SOCKADDR_IN6 = %d\n", sizeof(struct sockaddr_in6));
+  printf("SZ_SOCKADDR_IN6 = %zd\n", sizeof(struct sockaddr_in6));
 
   printf("\npublic export %%inline\n");
   printf("SZ_SOCKADDR : Bits32\n");
-  printf("SZ_SOCKADDR = %d\n", sizeof(struct sockaddr));
+  printf("SZ_SOCKADDR = %zd\n", sizeof(struct sockaddr));
 
   printf("\npublic export %%inline\n");
   printf("SZ_ADDRINFO : Bits32\n");
-  printf("SZ_ADDRINFO = %d\n", sizeof(struct addrinfo));
+  printf("SZ_ADDRINFO = %zd\n", sizeof(struct addrinfo));
 }
 
